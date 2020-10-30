9e43865a2391c5fd6fbdb5e23662ab533a5298f3
Jason Upchurch <jupchurch@fec.gov>
2020-10-30 11:29:40 -0400

    Test 'crashing' app

--- bin/run.sh
+++ bin/run.sh
@@ -1,2 +1,10 @@
+rand_mod=$(expr $RANDOM % 5)
+if [ $rand_mod -eq 0 ]; then
+    echo "Starting divisible by five app"
+else
+    echo "Crashing non-divisible by five app";
+    exit 1
+fi
+
 python manage.py cf_startup
 gunicorn --access-logfile - --error-logfile - --log-level info --timeout 300 -k gevent -w 9 webservices.rest:app
